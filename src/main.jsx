8bbf6c4429409756d401ef263841fe46239b54b8
Narendra Chelak <narendrachelak@gmail.com>
2026-06-07 13:16:27 +0530

    add query admin route

--- src/main.jsx
+++ src/main.jsx
@@ -5,22 +5,20 @@ import Admin from "./Admin.jsx";
 import AdminLogin from "./AdminLogin.jsx";
 
 const path = window.location.pathname;
+const params = new URLSearchParams(window.location.search);
+const page = params.get("page");
 const token = localStorage.getItem("satvapustiAdminToken");
 
 let Page = <App />;
 
-if (path === "/admin-login") {
+if (path === "/admin-login" || page === "admin-login") {
   Page = <AdminLogin />;
 }
 
-if (path === "/admin") {
-  Page = token === "admin_logged_in"
-    ? <Admin />
-    : <AdminLogin />;
+if (path === "/admin" || page === "admin") {
+  Page = token === "admin_logged_in" ? <Admin /> : <AdminLogin />;
 }
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    {Page}
-  </StrictMode>
+  <StrictMode>{Page}</StrictMode>
 );
